d78fd52d684f12884720120b6edc4b819f150f3c
Nikita Prokopov <prokopov@gmail.com>
2016-07-18 21:14:25 +0600

    removed Retina weight from webfonts

---
+++ release.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Remove Retina from webfonts
+rm distr/FiraCode-Retina.eot   2> /dev/null
+rm distr/FiraCode-Retina.woff  2> /dev/null
+rm distr/FiraCode-Retina.woff2 2> /dev/null
+
+# Move to folders
+mv distr/*.eot distr/eot/      2> /dev/null
+mv distr/*.woff distr/woff/    2> /dev/null
+mv distr/*.woff2 distr/woff2/  2> /dev/null
+mv distr/*.ttf distr/ttf/      2> /dev/null
+mv distr/*.otf distr/otf/      2> /dev/null
+
+# Install OTF version
+cp distr/otf/*.otf ~/Library/Fonts/
+
+# Pack zip archive
+cd distr
+find . -not -name ".*" | xargs zip ../FiraCode.zip
+cd ..
