6b13bc53d1cc79eb573b84f83b865a824f9d3f40
Nikita Prokopov <niki@tonsky.me>
2021-04-18 02:54:20 +0200

    Added U+EE06..U+EE0B Circular progress indicator `` #1182

--- script/progress.clj
+++ script/progress.clj
@@ -5,13 +5,24 @@
 (loop [i 0]
   (print
     (str
-      \u001b \u000d ;; ESC CR Moves the cursor to column zero
+      \u001b \u000d " " ;; ESC CR Moves the cursor to column zero
+
+      ;; normal
+      "["
+      (str/join
+        (for [j (range 2 17)]
+          (if (<= j i) "#" ".")))
+      "] "
+      (nth "|/-\\" (mod i 4))
+      " "
+
+      ;; Fira Code
       (if (= 0 i) \uee00 \uee03) ;; Progress start
       (str/join
         (for [j (range 2 17)]
           (if (<= j i) \uee04 \uee01)))
       (if (= 17 i) \uee05 \uee02)
-      "  "
+      " "
       (nth chars (mod i 6))
       " "
       (-> i (/ 17.0) (* 100) (int)) "%  "))
