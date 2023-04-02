cca7edd0a6cc8a54e398a308982c88b4d8bb2769
Kenneth, Cho Kai Hung <107766043+KennethCho-CKH@users.noreply.github.com>
2023-04-02 14:43:06 +0100

    Improve Modularity

--- clojure/fira_code/time.clj
+++ clojure/fira_code/time.clj
@@ -1,13 +1,12 @@
 (ns fira-code.time
   (:import
-   [java.time LocalDateTime ZoneId]
-   [java.time.format DateTimeFormatter]))
+   [java.time.LocalDateTime ZoneId]
+   [java.time.format.DateTimeFormatter]))
 
-
-(def ^ZoneId UTC (ZoneId/of "UTC"))
-
-
-(defn now-str []
+(defn current-time-str
+  "Returns a string representation of the current UTC time, in the format 'yyyy-MM-dd HH:mm:ss +0000'.
+   Optional `time-zone` argument can be provided to use a different time zone."
+  [& {:keys [time-zone] :or {time-zone ZoneId/UTC}}]
   (.format
-    (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss +0000")
-    (LocalDateTime/now UTC)))
+    (DateTimeFormatter/ofPattern "yyyy-MM-dd HH:mm:ss Z")
+    (LocalDateTime/now time-zone))))
