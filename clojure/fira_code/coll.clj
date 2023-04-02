0f1ae06297c42c491165cbf016c31b415ad59867
Kenneth, Cho Kai Hung <107766043+KennethCho-CKH@users.noreply.github.com>
2023-04-02 14:27:16 +0100

    Simplify the `multimap-by` Function

--- clojure/fira_code/coll.clj
+++ clojure/fira_code/coll.clj
@@ -1,25 +1,28 @@
-(ns fira-code.coll)
-
+(ns fira-code.coll
+  (:require
+   [clojure.string :as str]))
 
 (defn index-of [pred xs]
+  "Returns the index of the first element in xs that satisfies the predicate pred,
+  or nil if no such element is found."
   (let [res (reduce (fn [i x] (if (pred x) (reduced i) (inc i))) 0 xs)]
     (if (>= res (count xs))
-      -1
+      nil
       res)))
 
-
 (defn group-by-to [key-fn value-fn xs]
-  (reduce-kv
-    (fn [m k vs]
-      (assoc m k (value-fn vs)))
-    {}
-    (group-by key-fn xs)))
-
+  "Groups the elements in xs by applying the key function key-fn to each element,
+  and applies the value function value-fn to the resulting groups.
+  Returns a map of the form {k (value-fn vs)} where k is the result of applying
+  key-fn to a group of elements vs."
+  (->> xs
+       (group-by key-fn)
+       (reduce-kv (fn [m k vs]
+                    (assoc m k (value-fn vs))))))
 
 (defn multimap-by [f & kvs]
-  (reduce
-    (fn [m [k v]]
-      (if (contains? m k)
-        (update m k f v)
-        (assoc m k v)))
-    {} (partition 2 kvs)))
+  "Constructs a multimap by repeatedly applying the function f to a sequence of
+  key-value pairs. Returns a map where each key can have multiple values."
+  (reduce (fn [m [k v]]
+            (update m k f v))
+          {} kvs))
