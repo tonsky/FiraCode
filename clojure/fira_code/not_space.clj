c94f7aeb700be536137e94fe5276ea5f7bd7bdac
Kenneth, Cho Kai Hung <107766043+KennethCho-CKH@users.noreply.github.com>
2023-04-02 14:39:05 +0100

    Replace the `println` statement with proper logging

--- clojure/fira_code/not_space.clj
+++ clojure/fira_code/not_space.clj
@@ -1,8 +1,8 @@
 (ns fira-code.not-space
   (:require
    [clojure.string :as str]
+   [clojure.tools.logging :as log]
    [fira-code.glyphs :as glyphs]))
-
 
 (defn regen-not-space [font]
   (let [not-spaces (->> (:glyphs font)
@@ -10,5 +10,5 @@
                      (remove #(= "0" (:export %)))
                      (map :glyphname)
                      (sort))]
-    (println "  regenerated NotSpace:" (count not-spaces) "glyphs")
-    (glyphs/update-code font :classes "NotSpace" (constantly (str/join " " not-spaces)))))
+    (log/info "Regenerated NotSpace:" (count not-spaces) "glyphs")
+    (glyphs/update-code font :classes "NotSpace" (constantly (str/join " " not-spaces))))))
